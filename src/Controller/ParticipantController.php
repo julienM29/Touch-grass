3e077dc171966c09fe91cda9fd0e10669a115fe4
julienM29 <137764146+julienM29@users.noreply.github.com>
2026-05-20 10:27:56 +0200

    debut de ledit profil

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use App\Form\ProfilFormType;
 use App\Repository\TodoRepository;
+use App\Services\ParticipantService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class ParticipantController extends AbstractController
 {
+
+    public function __construct(
+        private ParticipantService $participantService
+    ) {}
+
     #[Route('/participant', name: 'app_participant')]
     public function index(): Response
     {
@@ -24,21 +32,43 @@ final class ParticipantController extends AbstractController
     {
         $user = $this->getUser();
 
-
         //       $eventsCreated = $eventRepository->countByOrganizer($user);
-//        $participations = $participationRepository->countByUser($user);
-//        $upcomingEvents = $eventRepository->countUpcomingByUser($user);
-//        $userEvents = $eventRepository->findByOrganizer($user);
+        //        $participations = $participationRepository->countByUser($user);
+        //        $upcomingEvents = $eventRepository->countUpcomingByUser($user);
+        //        $userEvents = $eventRepository->findByOrganizer($user);
         $upcomingEvents = 0;
         $userEvents = 0;
         $eventsCreated = 0;
         $participations = 0;
-        return $this->render('participant/index.html.twig', [
+        return $this->render('participant/detail.html.twig', [
             'user' => $user,
             'eventsCreated' => $eventsCreated,
             'participations' => $participations,
             'upcomingEvents' => $upcomingEvents,
             'userEvents' => $userEvents,
+        ]);
+    }
+
+    #[Route('/profil/modify', name: 'participant_profil_modify')]
+    #[IsGranted('ROLE_USER')]
+    public function modifyProfil( Request $request,  EntityManagerInterface $entityManager ): Response {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ProfilFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // 🖼️ image upload
+            $imageFile = $form->get('image')->getData();
+            if ($filename = $this->participantService->uploadImage($imageFile)) {
+                $user->setImage($filename);
+            }
+            $entityManager->flush();
+            return $this->redirectToRoute('participant_profil');
+        }
+
+        return $this->render('participant/detail.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/ProfilFormType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use App\Entity\Site;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ProfilFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('pseudo')
+            ->add('prenom')
+            ->add('nom')
+            ->add('telephone')
+
+            ->add('site', EntityType::class, [
+                'class' => Site::class,
+                'choice_label' => 'nom',
+            ])
+
+            ->add('image', FileType::class, [
+                'label' => 'Photo de profil',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2M',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez uploader une image valide',
+                    ])
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
